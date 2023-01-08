34c060ac4df0a0cf7c1a0cc152f9012132619a5a
Randall Adams <radams@myriad.com>
2023-01-08 16:15:53 -0700

    fix test

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -4,6 +4,6 @@ describe('sample render test', () => {
   });
 
   it('displays the title text', () => {
-    cy.get('h1').contains('Next.js Blog Theme');
+    cy.get('h1').contains('Brennan\'s Bike Shop');
   });
 });
