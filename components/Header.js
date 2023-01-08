ace6b9d0e99cbfa37237f198797da1b9fb9e2445
Randall Adams <radams@myriad.com>
2023-01-08 16:10:45 -0700

    update home page and theme

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,18 @@
 import Link from 'next/link';
+import Image from 'next/image'
 
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
+      <div className="header__logo">
+        <Link href="/">
+          <Image
+            src="/images/bbs_logo.jpeg"
+            alt="Brennan's Bike Shop"
+            width={345}
+            height={177} />
+        </Link>
+      </div>
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Brennan\'s Bike Shop';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Bike Shop in Lehi, Utah';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'Let\'s Go Shred';
 
   return {
     name,
