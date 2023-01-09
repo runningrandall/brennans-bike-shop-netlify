1c958ea402f17d9c79001a4de567d1936e7e7075
bikerbrennan <122237030+bikerbrennan@users.noreply.github.com>
2023-01-09 00:22:55 +0000

    updated home page and added services page

---
+++ pages/bike-services.js
@@ -0,0 +1,34 @@
+import Link from 'next/link';
+import { getPosts } from '../utils/mdx-utils';
+
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import Layout, { GradientBackground } from '../components/Layout';
+import ArrowIcon from '../components/ArrowIcon';
+import { getGlobalData } from '../utils/global-data';
+import SEO from '../components/SEO';
+
+export default function Index({ posts, globalData }) {
+  return (
+    <Layout>
+      <SEO title={globalData.name} description={globalData.blogTitle} />
+      <Header name={globalData.name} />
+      <main className="w-full">
+        <h1 className="text-3xl lg:text-5xl text-center mb-12">
+          Bike Services
+        </h1>
+        <p className='text-center'>
+          The following is a list of bike services that we offer.
+        </p>
+      </main>
+      <Footer copyrightText={globalData.footerText} />
+    </Layout>
+  );
+}
+
+export function getStaticProps() {
+  const posts = getPosts();
+  const globalData = getGlobalData();
+
+  return { props: { posts, globalData } };
+}
